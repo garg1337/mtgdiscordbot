8b9da9b3217cef9e599c0ed21f5b274172094e94
Arjun Garg <arjunpgarg@gmail.com>
2018-04-01 14:15:14 -0700

    Add embeddable function

--- bot.ts
+++ bot.ts
@@ -19,29 +19,43 @@ client.on('message', msg => {
         cardsPromise
         .then(cardResponses => {
             cardResponses.forEach(cardResponse => {
-                if (!cardResponse.imgOnly) {
-                    msg.channel.sendEmbed({
-                        title: `${cardResponse.card.name} ${cardResponse.card.mana_cost}`,
-                        description: cardResponse.card.oracle_text ? cardResponse.card.oracle_text : '',
-                        url: cardResponse.card.scryfall_uri,
-                        thumbnail: {
-                            url: cardResponse.card.image_uris.normal ? cardResponse.card.image_uris.normal : ''
-                        }
-                    })
-                } else {
-                    msg.channel.sendEmbed({
-                        title: `${cardResponse.card.name}`,
-                        url: cardResponse.card.scryfall_uri,
-                        image: {
-                            url: cardResponse.card.image_uris.normal ? cardResponse.card.image_uris.normal : ''
-                        }                    
-                })
-            }})})
+                let embed = getEmbeddableFromCardResponse(cardResponse);
+                console.log(embed);
+                msg.channel.send(embed);
+            })})
         .catch(error => {
-            msg.channel.send('No cards found =(');
+            msg.channel.send('An error occurred ' + error);
         });
     };
 });
+
+function getEmbeddableFromCardResponse(cardResponse: CardResponse): any {
+    if (!cardResponse.card) {
+        return {
+            description: cardResponse.notFoundError
+        };
+    }
+
+    if (!cardResponse.imgOnly) {
+        return {
+            title: `${cardResponse.card.name} ${cardResponse.card.mana_cost}`,
+            description: cardResponse.card.oracle_text ? cardResponse.card.oracle_text : '',
+            url: cardResponse.card.scryfall_uri,
+            thumbnail: {
+                url: cardResponse.card.image_uris.normal ? cardResponse.card.image_uris.normal : ''
+            }
+        }
+    }
+    else {
+        return {
+            title: `${cardResponse.card.name}`,
+            url: cardResponse.card.scryfall_uri,
+            image: {
+                url: cardResponse.card.image_uris.normal ? cardResponse.card.image_uris.normal : ''
+            }
+        }
+    }
+}
 
 client.login(Config.token);
 
@@ -51,11 +65,18 @@ function getResponsesForCards(cards: string[]): Promise<CardResponse[]> {
         responses.push(new Promise((resolve,reject) => {
             let imgOnly = card.indexOf('!') === 0;
             let cardForSearch = imgOnly ? card.slice(1) : card;
-            Scry.Cards.byName(cardForSearch).then(response => {
+            Scry.Cards.byName(cardForSearch)
+            .then(response => {
                 resolve({
                     imgOnly: imgOnly,
                     card: response
                 });    
+            })
+            .catch(error => {
+                resolve({
+                    imgOnly: imgOnly,
+                    notFoundError: error
+                });
             })
         }));
     });
@@ -65,7 +86,8 @@ function getResponsesForCards(cards: string[]): Promise<CardResponse[]> {
 
 interface CardResponse {
     imgOnly: boolean,
-    card: Scry.Card
+    card?: Scry.Card,
+    notFoundError?: string
 }
 
 function getCards(str: string): string[] {
